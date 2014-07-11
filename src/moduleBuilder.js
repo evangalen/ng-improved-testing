2c3dc8d39fdbc0c90a6e6ba653ab561cac408ab7
evangalen <emil.van.galen@gmail.com>
2014-07-11 11:38:15 +0200

    The ModuleBuilder is almost fully functional. We need to finalize its API (adding some more various of methods) and then test it extensively.

--- src/moduleBuilder.js
+++ src/moduleBuilder.js
@@ -11,7 +11,7 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
      */
     function ModuleBuilder(moduleName) {
 
-        function registerComponent(type, componentName, componentKind, dependenciesUsage, dependencies) {
+        function includeComponent(type, componentName, componentKind, dependenciesUsage, dependencies) {
             var toBeIncludedModuleComponent = {
                 type: type,
                 componentName: componentName,
@@ -25,8 +25,6 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
 
             toBeIncludedModuleComponents.push(toBeIncludedModuleComponent);
         }
-
-        var servicesUsingMockedServices = [];
 
         /** @type {angular.Module} */
         var originalModule = angular.module(moduleName);
@@ -49,6 +47,12 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
         /** @type {Object.<ModuleBuilder.ToBeIncludedModuleComponent>} */
         var toBeIncludedModuleComponents = [];
 
+        //TODO: comment
+        this.serviceWithMocks = function(serviceName) {
+            includeComponent('service', serviceName, 'withMocks');
+            return this;
+        };
+
         /**
          * Includes a service that replaces the dependencies specified in <em>toBeMockedDependencies</em> with mock
          * implementations.
@@ -60,7 +64,7 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
          * @returns {moduleIntrospectorFactory.ModuleBuilder} the module builder instance
          */
         this.serviceWithMocksFor = function(serviceName, toBeMockedDependencies) {
-            registerComponent('service', serviceName, 'withMocks', 'for', toBeMockedDependencies);
+            includeComponent('service', serviceName, 'withMocks', 'for', toBeMockedDependencies);
             return this;
         };
 
@@ -75,7 +79,7 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
          * @returns {moduleIntrospectorFactory.ModuleBuilder} the module builder instance
          */
         this.filterWithMocksFor = function(filterName, toBeMockedDependencies) {
-            registerComponent('filter', filterName, 'withMocks', 'for', toBeMockedDependencies)
+            includeComponent('filter', filterName, 'withMocks', 'for', toBeMockedDependencies);
             return this;
         };
 
@@ -86,20 +90,9 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
          * @returns {moduleIntrospectorFactory.ModuleBuilder}
          */
         this.filterAsIs = function(filterName) {
-            registerComponent('filter', filterName, 'actual');
+            includeComponent('filter', filterName, 'asIs');
             return this;
         };
-
-//        /**
-//         * Including a mocked filter (instead of the actual one) in the module
-//         *
-//         * @param {string} filterName name of the filter to be included in the to be build module
-//         * @returns {moduleIntrospectorFactory.ModuleBuilder}
-//         */
-//        this.WithMockedFilter = function(filterName) {
-//            registrations.push({type: RegistrationType.MOCKED_FILTER, name: filterName});
-//            return this;
-//        };
 
         /**
          * Includes a controller that uses mocked service dependencies (instead of actual services) in the module.
@@ -109,7 +102,7 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
          * @returns {moduleIntrospectorFactory.ModuleBuilder}
          */
         this.controllerWithMocksFor = function(controllerName, toBeMockedDependencies) {
-            registerComponent('controller', controllerName, 'withMocks', 'for', toBeMockedDependencies);
+            includeComponent('controller', controllerName, 'withMocks', 'for', toBeMockedDependencies);
             return this;
         };
 
@@ -120,117 +113,173 @@ function moduleIntrospectorFactory(moduleIntrospector, mockCreator) {
          * @returns {moduleIntrospectorFactory.ModuleBuilder}
          */
         this.controllerAsIs = function(controllerName) {
-            registerComponent('controller', controllerName, 'actual');
+            includeComponent('controller', controllerName, 'asIs');
             return this;
         };
-
-//        /**
-//         * Including a mocked controller (instead of the actual one) in the module
-//         *
-//         * @param {string} controllerName name of the controller to be included in the to be build module
-//         * @returns {moduleIntrospectorFactory.ModuleBuilder}
-//         */
-//        this.withMockedController = function(controllerName) {
-//            //TODO: make this the to instantiate "inside" the tests you need to suffix "Mock" but not inside a
-//            //  "real" service; inside a test you need "$controller('...CtrlMock', {$scope: ...})" but in a "real"
-//            //  service the following should still work: "$controller('....Ctrl', ....);"
-//            //TODO: determine how a test obtains the mock for the Controller constructor function;
-//            //  a controllers isn't registered on $provide at all!
-//            //  alternative... still "mocked" controller support and allow a callback with $provide on the
-//            //  "moduleBuilder" (just like the "angular.mock.module" function)
-//
-//            registrations.push({type: RegistrationType.MOCKED_CONTROLLER, name: controllerName});
-//            return this;
-//        };
-
-//        //TODO: also add: this.withDirective
-//        /**
-//         * @param {string} directiveName
-//         * @returns {ModuleBuilder}
-//         */
-//        this.withDirectiveUsingMockedServices = function() {
-//            throw 'not implemented yet';
-//        };
-//
-//        /**
-//         * @param {string} directiveName
-//         * @param {...string} except
-//         * @returns {ModuleBuilder}
-//         */
-//        this.withDirectiveUsingMockedServicesExcept = function() {
-//            throw 'not implemented yet';
-//        };
-//
-//        //NOT IMPLEMENTED: withMockedDirective
-//        // not commonly needed... can always be made using the "function($provide)" callback
 
         /**
          * Builds ...
          * @returns {Function}
          */
         this.build = function() {
+
+            function getModuleComponentDependencies(type, name) {
+                var result;
+
+                if (type === 'service') {
+                    result = introspector.getServiceDependencies(injector, name);
+                } else if (type === 'controller') {
+                    result = introspector.getControllerDependencies(injector, name);
+                } else if (type === 'filter') {
+                    result = introspector.getFilterDependencies(injector, name);
+                } else {
+                    throw 'Unsupported module component type: ' + type;
+                }
+
+                return result;
+            }
+
+            function getDeclaredModuleComponent(type, name) {
+                var result;
+
+                if (type === 'service') {
+                    result = introspector.getServiceDeclaration(name);
+                } else if (type === 'controller') {
+                    result = introspector.getControllerDeclaration(name);
+                } else if (type === 'filter') {
+                    result = introspector.getFilterDeclaration(name);
+                } else {
+                    throw 'Unsupported module component type: ' + type;
+                }
+
+                return result;
+            }
+
+            function handleAsIsComponentKind(toBeIncludedModuleComponent) {
+                var type = toBeIncludedModuleComponent.type;
+                var name = toBeIncludedModuleComponent.componentName;
+
+                if (type === 'controller' || type === 'filter') {
+                    var dependencies = getModuleComponentDependencies(type, name);
+
+                    angular.forEach(dependencies, function(dependencyInfo, dependencyName) {
+                        asIsServices[dependencyName] = dependencyInfo.instance;
+                    });
+
+                    var declaredModuleComponent = getDeclaredModuleComponent(type, name);
+
+                    declarations[name] = {
+                        provideMethod: declaredModuleComponent.providerMethod,
+                        declaration: declaredModuleComponent.declaration
+                    };
+                } else if (type === 'service') {
+                    asIsServices[name] = injector.get(name);
+                } else {
+                    throw 'Unsupported module component type: ' + type;
+                }
+            }
+
+            function handleWithMocksComponentKind(toBeIncludedModuleComponent) {
+                var type = toBeIncludedModuleComponent.type;
+                var name = toBeIncludedModuleComponent.componentName;
+
+                var dependencies = getModuleComponentDependencies(type, name);
+
+                var declaredModuleComponent = getDeclaredModuleComponent(type, name);
+                var declaration = declaredModuleComponent.declaration;
+
+                var annotatedDeclaration = [];
+
+                angular.forEach(dependencies, function (dependencyInfo, dependencyName) {
+                    var shouldBeMocked = dependencyShouldBeMocked(toBeIncludedModuleComponent, dependencyName);
+                    var canBeMocked = mockCreator.canBeMocked(dependencyInfo.instance);
+
+                    console.log(dependencyName + ' shouldBeMocked=' + shouldBeMocked);
+                    console.log(dependencyName + ' canBeMocked=' + canBeMocked);
+
+                    if (shouldBeMocked && !canBeMocked &&
+                            toBeIncludedModuleComponent.dependenciesUsage === 'for') {
+                        throw 'Could not mock the dependency explicitly asked to mock: ' + dependencyName;
+                    }
+
+                    var toBeMocked = dependencyShouldBeMocked && canBeMocked;
+
+                    if (toBeMocked) {
+                        mockedServices[dependencyName] = dependencyInfo.instance;
+                    } else {
+                        asIsServices[dependencyName] = dependencyInfo.instance;
+                    }
+
+                    annotatedDeclaration.push(dependencyName + (toBeMocked ? 'Mock' : ''));
+                });
+
+                var originalNonAnnotatedServiceDeclaration;
+                if (angular.isArray(declaration)) {
+                    originalNonAnnotatedServiceDeclaration = declaration[declaration.length - 1];
+                } else {
+                    originalNonAnnotatedServiceDeclaration = declaration;
+                }
+
+                annotatedDeclaration.push(originalNonAnnotatedServiceDeclaration);
+
+                declarations[name] = {
+                    providerMethod: declaredModuleComponent.providerMethod,
+                    declaration: annotatedDeclaration
+                };
+            }
+
+            function dependencyShouldBeMocked(toBeIncludedModuleComponent, dependencyName) {
+                var dependenciesUsage = toBeIncludedModuleComponent.dependenciesUsage;
+
+                if (dependenciesUsage === 'for') {
+                    return toBeIncludedModuleComponent.dependencies.indexOf(dependencyName) !== -1;
+                } else if (dependenciesUsage === 'except') {
+                    return toBeIncludedModuleComponent.dependencies.indexOf(dependencyName) === -1;
+                } else if (!dependenciesUsage) {
+                    return true;
+                } else {
+                    throw 'Invalid dependencies usage: ' + dependenciesUsage;
+                }
+            }
+
             numberOfBuildModules += 1;
+
             var buildModuleName = 'generatedByNgImprovedTesting#' + numberOfBuildModules;
 
-            /** @type Array.<string> */
-            var toBeMockedServices = [];
+            /** @type Object.<Object> */
+            var mockedServices = {};
 
-            var nonMockServiceDependencies = [];
+            /** @type Object.<Object> */
+            var asIsServices = {};
+
+            /**
+             * @type {Object.<{$providerMethod: string, declaration: (Function|Array.<(string|Function)>)}>}
+             */
+            var declarations = {};
 
             var buildModule = angular.module(buildModuleName, originalModule.requires);
 
-
-
-            moduleServices = {
-                'a': 'actual',
-                'b': 'mock',
-                'c': 'actualAndMock'
-            };
-
-
-            angular.forEach(servicesUsingMockedServices, function (serviceName) {
-                var serviceDependencies = introspector.getServiceDependencies(injector, serviceName);
-
-                var annotatedService = [];
-
-                angular.forEach(serviceDependencies, function (serviceDependencyInfo, serviceDependencyName) {
-                    var toBeMocked = angularModuleNames.indexOf(serviceDependencyInfo.module.name) === -1 &&
-                        mockCreator.canBeMocked(serviceDependencyInfo.instance);
-
-                    if (toBeMocked) {
-                        toBeMockedServices.push(serviceDependencyName);
-                    } else {
-                        nonMockServiceDependencies.push(serviceDependencyName);
-                    }
-
-                    annotatedService.push(serviceDependencyName + (toBeMocked ? 'Mock' : ''));
-                });
-
-                var serviceDeclaration = introspector.getServiceDeclaration(serviceName);
-
-                var originalNonAnnotatedServiceDeclaration;
-                if (angular.isArray(serviceDeclaration.declaration)) {
-                    originalNonAnnotatedServiceDeclaration =
-                        serviceDeclaration.declaration[serviceDeclaration.declaration.length - 1];
-                } else {
-                    originalNonAnnotatedServiceDeclaration = serviceDeclaration.declaration;
+            angular.forEach(toBeIncludedModuleComponents, function (toBeIncludedModuleComponent) {
+                if (toBeIncludedModuleComponent.componentKind === 'asIs') {
+                    handleAsIsComponentKind(toBeIncludedModuleComponent);
+                } else if (toBeIncludedModuleComponent.componentKind === 'withMocks') {
+                    handleWithMocksComponentKind(toBeIncludedModuleComponent);
                 }
-
-                annotatedService.push(originalNonAnnotatedServiceDeclaration);
-
-                buildModule[serviceDeclaration.providerMethod](serviceName, annotatedService);
             });
 
             return angular.mock.module(function($provide) {
-                angular.forEach(toBeMockedServices, function(toBeMockService) {
-                    var originalService = injector.get(toBeMockService);
-
-                    var serviceMock = mockCreator.createMock(originalService);
-                    $provide.value(toBeMockService + 'Mock', serviceMock);
+                angular.forEach(mockedServices, function(originalService, serviceName) {
+                    var mockedService = mockCreator.createMock(originalService);
+                    $provide.value(serviceName + 'Mock', mockedService);
                 });
 
-                angular.forEach(nonMockServiceDependencies, function(nonMockServiceDependency) {
-                    $provide.value(nonMockServiceDependency, injector.get(nonMockServiceDependency));
+                angular.forEach(asIsServices, function(originalService, serviceName) {
+                    $provide.value(serviceName, originalService);
+                });
+
+                angular.forEach(declarations, function(declarationInfo, declarationName) {
+                    console.log(declarationInfo.declaration);
+                    $provide[declarationInfo.providerMethod](declarationName, declarationInfo.declaration);
                 });
             }, buildModule.name);
         };
