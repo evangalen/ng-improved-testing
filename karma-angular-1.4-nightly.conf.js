7ade422bbb506ac43c6a8d39595256eb9f81df8f
Emil van Galen <emil.van.galen@gmail.com>
2015-01-19 23:09:16 +0100

    Our Jasmine tests are now also executed against the latest nightly build of AngularJS 1.4 (to ensure that ngImprovedTesting remains compatible with the upcoming AngularJS 1.4).

---
+++ karma-angular-1.4-nightly.conf.js
@@ -0,0 +1,26 @@
+var sharedConfig = require('./karma-shared.conf');
+
+module.exports = function(config) {
+    sharedConfig(config, {testName: 'ngImprovedTesting: AngularJS 1.3.x', logFile: 'karma-angular-1.3.log'});
+
+    config.set({
+
+        // list of files / patterns to load in the browser
+        files: [
+            'bower_components/angular-1.4-nightly/angular.js',
+            'bower_components/ng-module-introspector/ng-module-introspector.js',
+            'bower_components/angular-mocks-1.4-nightly/angular-mocks.js',
+            'src/module.js',
+            'src/config.js',
+            'src/mockCreator.js',
+            'src/moduleBuilder.js',
+            'src/mockInject.js',
+            'src/$q.js',
+            'src/exports.js',
+            'bower_components/angular-animation-1.3/angular-animate.js',
+            'test/!(moduleDeclaredAfterLoadedAllSpecs).js',
+            'test/moduleDeclaredAfterLoadedAllSpecs.js'
+        ]
+
+    });
+};
